9909b2f8d27434293bc2396962a71248123d7e55
institutoDomDeDeus <institutodomdedeus.ti@gmail.com>
2024-09-24 04:53:05 -0300

    Update base.js

--- base.js
+++ base.js
@@ -5,5 +5,36 @@ document.querySelector('#pix-copy a').addEventListener('click', function (e) {
       alert('Chave Pix copiada com sucesso!');
     }, function (err) {
       console.error('Erro ao copiar a chave Pix: ', err);
-    });
   });
+});
+
+
+function scrollToTop() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+
+window.onscroll = function() {
+  const btnContainer = document.getElementById("backToTopContainer");
+  const footer = document.querySelector('footer');
+  const backToTopText = document.getElementById("backToTopText");
+
+
+  const footerRect = footer.getBoundingClientRect();
+  const windowHeight = window.innerHeight;
+
+
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    btnContainer.style.display = "block";
+  } else {
+    btnContainer.style.display = "none";
+  }
+
+
+  if (footerRect.top <= windowHeight) {
+    backToTopText.style.color = "#FFFFFF"; 
+  } else {
+    backToTopText.style.color = "#0E0B86";
+  }
+};
+
